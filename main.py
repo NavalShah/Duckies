ad62346b78596c084bdea9c922047f9e7749f162
arsenic3375 <denis.v.arsenev@gmail.com>
2024-12-24 17:47:36 -0500

    Did some diging on how the form input is constructed, found the form input

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+import requests
+from bs4 import BeautifulSoup
+
+'''Home screen'''
+url = "https://parents.sbschools.org/genesis/sis/view?gohome=true"
+
+'''url for password entry?'''
+url2 = "https://parents.sbschools.org/genesis/sis/j_security_check?parents=Y"
+
+studentID = ""
+
+r = requests.post(url2,data={'username': 'value', 'password': 'value'})
+print(r)
